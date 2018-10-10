e60bce0c5f3c920c66c056c18ba669ef0a5a6ebb
Greg Smith <gs@tether.com>
2018-10-10 15:21:51 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import data from './data/bookdata.json';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+		super(props);
+		this.state= {
+      books: [],
+      wishlist: []
+		};
+		//binding all our functions to this class
+		// this.deleteComment = this.deleteComment.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log(e.currentTarget.id)
+  }
+
+  componentDidMount(){
+    this.setState({ books: data})
+  }
   render() {
+    const books = this.state.books.map((book) => <li id={ book.id } key={book.guid} onClick={this.handleClick}>{ book.name }</li>)
+    const whishlist = this.state.wishlist.map((book) => <li key={book.guid}>{ book.name }</li>)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Zonar Books</h1>
         </header>
+        <div className="books list">
+          <h2>Books</h2>
+          <ul>
+            { books }
+          </ul>
+        </div>
+
+        <div className="wish listy">
+          <h2>Whishlist</h2>
+          <ul>
+            { whishlist }
+          </ul>
+        </div>
       </div>
     );
   }
